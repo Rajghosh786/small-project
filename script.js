7256087643a65899470d06ab067868169f53859d
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2024-11-18 02:12:17 +0530

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-let n = 6
+let x = 6
 
-if(n%2 === 0){
+if(x%2 === 0){
     console.log("Even")
 }else{
     console.log("Odd")
+}
+
+
+let n = 10
+
+for(i = 1; i <= n;i++){
+  let bag = ''
+  for(j = 1; j <= n;j++){
+    // bag += "*"
+    if(i === j){
+      bag+= "*"
+    }
+    else if(j === 1){
+      bag+= "*"
+    } else if(j === 10){
+      bag+= "*"
+    }
+    else{
+      bag+= " "
+    }
+  }
+  console.log(bag)
 }
